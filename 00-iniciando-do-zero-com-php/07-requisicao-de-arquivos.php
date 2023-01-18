a636de534294b4172ad6dc27065737206761c79b
waguirra <walisson.aguirra@gmail.com>
2023-01-18 01:34:32 -0300

    Requisição de arquivos

---
+++ 00-iniciando-do-zero-com-php/07-requisicao-de-arquivos.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once dirname(__DIR__) . '/stackphp/stackphp.php';
+
+_title('Requisição de arquivos');
+
+/*
+ * [ include ] https://php.net/manual/pt_BR/function.include.php
+ * [ include_once ] https://php.net/manual/pt_BR/function.include-once.php
+ */
+_line('include, include_once', __LINE__);
+
+//include 'file.php';
+//echo '<p>Continue</p>';
+
+/*
+ * [ require ] https://php.net/manual/pt_BR/function.require.php
+ * [ require_once ] https://php.net/manual/pt_BR/function.require-once.php
+ */
+_line('require, require_once', __LINE__);
+
+//require 'file.php';
+//echo '<p>Continue</p>';
+
