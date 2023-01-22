3335ad9bf6745cbf934c330d851988dd0360a4d3
waguirra <walisson.aguirra@gmail.com>
2023-01-21 23:33:26 -0300

    Funções para arrays

---
+++ 01-manipulacao-e-tratamento/01-funcoes-para-arrays.php
@@ -0,0 +1,67 @@
+<?php
+require dirname(__DIR__) . '/stackphp/stackphp.php';
+_title('Funções para arrays');
+
+/** [ criação ] https://php.net/manual/pt_BR/ref.array.php */
+_line('manipulação', __LINE__);
+
+$index = [
+    'laranja',
+    'maça',
+    'pera'
+];
+
+$assoc = [
+    'fruta_1' => 'laranja',
+    'fruta_2' => 'maça',
+    'fruta_3' => 'pera'
+];
+
+array_unshift($index, 'manga', 'morango');
+
+$assoc = ['fruta_4' => 'manga', 'fruta_5' => 'morango'] + $assoc;
+
+array_push($index, '');
+
+$assoc = $assoc + ['fruta_6' => ''];
+
+array_shift($index);
+array_shift($assoc);
+
+array_pop($index);
+array_pop($assoc);
+
+$index = array_filter($index);
+$assoc = array_filter($assoc);
+
+var_dump($index, $assoc);
+
+/** [ ordenação ] reverse | asort | ksort | sort */
+_line('ordenação', __LINE__);
+
+$index = array_reverse($index);
+$assoc = array_reverse($assoc);
+
+asort($index);
+asort($assoc);
+
+ksort($index);
+ksort($assoc);
+
+krsort($index);
+krsort($assoc);
+
+sort($index);
+
+var_dump($index, $assoc);
+
+/** [ verificação ]  keys | values | in | explode */
+_line('verificação', __LINE__);
+
+var_dump(array_keys($assoc), array_values($assoc));
+
+$string = implode(', ', $assoc);
+
+if (in_array('laranja', $assoc)) {
+    echo '<p>{$string}</p>';
+}
